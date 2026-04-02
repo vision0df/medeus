a26434242d72a637689ca67252cb2f8fc19a3fb0
vision0df <vision0df@gmail.com>
2026-04-02 15:34:26 +0300

    stable ver

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from google import genai
 from PIL import Image
+from google.genai import types
 
 app = Flask(__name__)
 CORS(app)
